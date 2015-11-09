532081c9e4d1ce9c64232544c0cdc78a312f4f64
Dauntless- <eirik_stub@hotmail.com>
2015-11-09 14:02:34 +0100

    test

---
+++ Unity/Assets/Scripts/Enhet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enhet {
+
+	private string enhetsId;
+	private Data[] datas;
+
+	public Enhet(string enhetsId, Data[] datas){
+		this.enhetsId = enhetsId;
+		this.datas = datas;
+	}
+
+	public string getEnhetsId(){
+		return enhetsId;
+	}
+
+	public Data[] getAllData(){
+		return datas;
+	}
+
+	public Data getDataByKey(string key){
+		for(int i=0; i<datas.Length; i++){
+			if(datas[i].getDataType().Equals(key)){
+				return datas[i];
+			}
+		}
+		return null;
+	}
+	
+}
---
+++ Unity/Assets/Scripts/Lokalitet.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lokalitet {
+
+	private int lokalitetsnummer;
+	private string lokalitetsnavn;
+	private double breddegrad;
+	private double lengdegrad;
+	private Enhet[] enheter;
+
+	public Lokalitet(int lokalitetsnummer, string lokalitetsnavn, double breddegrad, double lengdegrad, Enhet[] enheter){
+		this.lokalitetsnummer = lokalitetsnummer;
+		this.lokalitetsnavn = lokalitetsnavn;
+		this.breddegrad = breddegrad;
+		this.lengdegrad = lengdegrad;
+		this.enheter = enheter;
+	}
+
+	public int getLokalitetsnummmer(){
+		return lokalitetsnummer;
+	}
+
+	public string getLokalitetsnavn(){
+		return lokalitetsnavn;
+	}
+
+	public double getBreddegrad(){
+		return breddegrad;
+	}
+
+	public double getLengdegrad(){
+		return lengdegrad;
+	}
+
+	public Enhet[] getEnheter(){
+		return enheter;
+	}
+
+	public Enhet getEnhetById(string id){
+		for(int i=0; i<enheter.Length; i++){
+			if(enheter[i].getEnhetsId().Equals(id)){
+				return enheter[i];
+			}
+		}
+		return null;
+	}
+}
