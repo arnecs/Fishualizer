4a69e6303d89279f67e620404e3dd49602981bae
arnecs <achrs@hotmail.com>
2016-01-08 16:21:11 +0100

    ExcelDataReader

--- Unity/Assets/Scripts/EXCELREADER.cs
+++ Unity/Assets/Scripts/EXCELREADER.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿// Hentet fra: https://github.com/ExcelDataReader/ExcelDataReader
+// den 08/01-16 kl. 15:00
+
+
+
+
+using UnityEngine;
 using System.Collections;
 using System; 
 using System.IO; 
 using System.Data;
 using Excel;
+using System.Collections.Generic;
 
 
  
@@ -20,33 +27,74 @@ public class EXCELREADER : MonoBehaviour {
 
 	}
 
-	void readXLS( string filePath)
+	List<Lokalitet> readXLS( string filePath)
 	{
 
-		FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
-
-		//1. Reading from a binary Excel file ('97-2003 format; *.xls)
-		IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+		Dictionary<string, Lokalitet> lokDict = new Dictionary<string, Lokalitet> ();
+		Dictionary<string, int> headers = new Dictionary<string, int> ();
 
 
 
-		//3. DataSet - The result of each spreadsheet will be created in the result.Tables
-		//DataSet result = excelReader.AsDataSet();
+		try {
+			FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
 
-		//4. DataSet - Create column names from first row
-		//excelReader.IsFirstRowAsColumnNames = true;
-		//DataSet result = excelReader.AsDataSet();
+			//1. Reading from a binary Excel file ('97-2003 format; *.xls)
+			IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
 
-		//5. Data Reader methods
-		while (excelReader.Read())
-		{
-			for (int i = 0; i < excelReader.FieldCount; i++) {
-				
-				print (excelReader.GetString (i));
+			excelReader.Read();
+			if (excelReader.Read()) {
+				for (int i = 0; i < excelReader.FieldCount; i++) {
+					headers.Add(excelReader.GetString(i), i);
+
+				}
 			}
-		}
+			
 
-		//6. Free resources (IExcelDataReader is IDisposable)
-		excelReader.Close();
-	}
+			while (excelReader.Read())
+			{
+				int lokIdIndeks = -1;
+				Lokalitet lok = null;
+				string lokNavn;
+
+				// Finn riktig lokalitet
+				if (headers.TryGetValue("Lokalitet", out lokIdIndeks)) {
+					lokNavn = excelReader.GetString(lokIdIndeks);
+
+					if (!lokDict.TryGetValue(lokNavn, out lok)) {
+
+						lok = new Lokalitet();
+						lok.setLokalitetsNavn(lokNavn);
+
+					}
+				}
+
+				Enhet enhet;
+				int enhetIndeks;
+				string enhetId;
+
+				// Finn riktig enhet
+				if (headers.TryGetValue("Enhet", out enhetIndeks)) {
+					enhetId = excelReader.GetString(enhetIndeks);
+
+					enhet = lok.getEnhetById(enhetId);
+
+					if (enhet == null) {
+						enhet = lok.leggTilEnhet(enhetId);
+					}
+				}
+					
+				for (int i = 0; i < excelReader.FieldCount; i++) {
+					
+
+				}
+			}
+
+			//6. Free resources (IExcelDataReader is IDisposable)
+			excelReader.Close();
+			} catch (Exception e) {
+				
+			}
+
+		return null;
+		}
 }
