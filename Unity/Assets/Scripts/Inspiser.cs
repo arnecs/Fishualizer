b6c14befb4a183891d202743ce2bc0dc0b19cfd5
JorgenNyborg <jorgen.e.nyborg@gmail.com>
2016-01-09 18:18:06 +0100

    Inspisererer

---
+++ Unity/Assets/Scripts/Inspiser.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inspiser : MonoBehaviour {
+
+	bool isActive;
+	Lokalitet l;
+
+	// Use this for initialization
+	void Start () {
+		l = (Lokalitet)this.gameObject.GetComponent<OnlineMapsMarker3DInstance>().marker.customData;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void toggleIsActive(){
+		if(isActive){
+			isActive = false;
+		}else{
+			isActive = true;
+		}
+	}
+
+	void OnMouseOver(){
+		if (Input.GetMouseButtonDown (0)) {
+			// Whatever you want it to do.
+			Debug.Log (l.getEnheter().Count);
+			toggleIsActive ();
+		}
+	}
+
+	void OnGUI(){
+		if (isActive) {
+			var point = Camera.main.WorldToScreenPoint(transform.position);
+			GUI.Box (new Rect (point.x + Screen.width/20, Screen.height-point.y - Screen.height/5, 100, 20), "Tittel");
+		}
+	}
+}
