c90b7c1c3f8f1c4c7e38dc75e6657e9ac09a92cd
JorgenNyborg <jorgen.e.nyborg@gmail.com>
2016-01-10 14:56:49 +0100

    Fortsetter arbeidet i Inspiseringen

--- Unity/Assets/Scripts/InspiserLokalitet.cs
+++ Unity/Assets/Scripts/InspiserLokalitet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class InspiserLokalitet : MonoBehaviour {
@@ -11,8 +12,6 @@ public class InspiserLokalitet : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Starter");
-		//g = this.gameObject;
-		//g.gameObject.GetComponent<InspiserLokalitet> ().enabled = true;
 		l = (Lokalitet)gameObject.GetComponent<OnlineMapsMarker3DInstance>().marker.customData;
 	}
 	
@@ -25,27 +24,24 @@ public class InspiserLokalitet : MonoBehaviour {
 		if(showTooltip){
 			showTooltip = false;
 		}else{
-			//l = (Lokalitet)gameObject.GetComponent<OnlineMapsMarker3DInstance>().marker.customData;
-			//Debug.Log(l.getLokalitetsnavn());
 			showTooltip = true;
 		}
 	}
 	
 	void OnMouseOver(){
 		if (Input.GetMouseButtonDown (0)) {
-			// Whatever you want it to do.
 			toggleTooltip ();
 		}
-		
-		
 	}
 
 	void OnGUI(){
+		GUIStyle style = new GUIStyle(GUI.skin.label);
+		style.alignment = TextAnchor.MiddleCenter;
+
 		var point = Camera.main.WorldToScreenPoint(transform.position);
 		if (showTooltip) {
 			GUI.Box (new Rect (point.x + Screen.width/20, Screen.height-point.y - Screen.height/5, 100, 20), l.getLokalitetsnavn());
 		}
-
-		GUI.TextField (new Rect (point.x, Screen.height - point.y + Screen.height / 20, 100, 20), l.getLokalitetsnavn (), GUIStyle.none);
+		GUI.Label (new Rect (point.x, Screen.height - point.y + Screen.height / 20, 100, 20), l.getLokalitetsnavn (), style);
 	}
 }
