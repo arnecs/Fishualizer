b3f89b31277bd2c98ffa0d1444f1e79bdf0928f0
arnecs <achrs@hotmail.com>
2016-01-12 12:51:05 +0100

    Feilmelding uferdig

--- Unity/Assets/Scripts/Feilmelding.cs
+++ Unity/Assets/Scripts/Feilmelding.cs
@@ -6,13 +6,13 @@ public class Feilmelding : MonoBehaviour {
 	public float width;
 	public float height;
 
-	bool show = true;
-	string text = "Kunne ikke lese fil";
+	bool show;
+	string text = "";
 
 
 	// Use this for initialization
 	void Start () {
-	
+		Show("Lorem ispum dolor sit amet");
 	}
 	
 	// Update is called once per frame
@@ -25,18 +25,23 @@ public class Feilmelding : MonoBehaviour {
 
 		if (show) {
 			var bounds = new Rect ((Screen.width - width) / 2, (Screen.height - height) / 2, width, height);
-
 			GUI.Box (bounds, "Feil");
 
+			/* Text */
+			GUI.skin.label.alignment = TextAnchor.UpperCenter;
+		
+			GUI.Label (new Rect ((Screen.width - width) / 2 + 20, (Screen.height - height) / 2 + 50, width - 40, height - 100), text);
 
 
-			// Ok Bottun
+			/* Ok Button */
 			var buttonWidth = 100;
 			var buttonHeight = 30;
 
 			var okButtonRect = new Rect ((Screen.width - buttonWidth) / 2, (Screen.height + height) / 2 - buttonHeight - 20, buttonWidth, buttonHeight);
 			if (GUI.Button (okButtonRect, "Ok")) {
 				show = false;
+				var mapControl = GameObject.Find ("Tileset map").GetComponent<OnlineMapsControlBase3D> ();
+				mapControl.allowUserControl = true;
 			}
 		}
 
@@ -44,6 +49,11 @@ public class Feilmelding : MonoBehaviour {
 
 
 	public void Show(string melding) {
+		show = true;
+		text = melding;
+
+		var mapControl = GameObject.Find ("Tileset map").GetComponent<OnlineMapsControlBase3D> ();
+		mapControl.allowUserControl = false;
 
 	}
 }
